e069500f420ec84b108b5f6d687111976365fc43
Monica Alba <soeur_azmaria@hotmail.com>
2019-07-12 09:36:24 -0500

    tomorrows weather added

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
         }else if (body.error){
             callback('Unable to find location', undefined)
         }else{
-            callback(undefined, body.daily.data[0].summary + ' La temperatura actual es ' +body.currently.temperature + ' grados. Hay una probabilidad de ' + body.currently.precipProbability + '% de lluvia')
+            callback(undefined, body.daily.data[0].summary + ' La temperatura actual es ' +body.currently.temperature + ' grados. Hay una probabilidad de ' + body.currently.precipProbability + '% de lluvia. Mañana estará: ' + body.daily.data[1].summary)
         }
     })
 
